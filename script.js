153e367c90606b88686509b2e861356e098f210c
sachiraghab <sachiraghab@gmail.com>
2023-09-17 02:04:50 +0530

    frontend part complete

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-const primaryColor = '#fff';
-const secondary = '#f0f0f6';
-const yellowBG = '#ffb400';
+window.onload = function (){
+  // const primaryColor = '#fff';
+  // const secondary = '#f0f0f6';
+  // const yellowBG = '#ffb400';
 
-function makeActive(){
-  document.getElementById('icon').style.backgroundColor = yellowBG;
+  var menuHeader = document.getElementById("header-elem");
+  var menuItems = menuHeader.getElementsByClassName("icon-container");
+  for(var i=0; i<menuItems.length; i++){
+    menuItems[i].addEventListener("click",function (){
+      var currentItem = document.getElementsByClassName("active");
+      currentItem[0].className = currentItem[0].className.replace("active","");
+      this.className = this.className + " active";
+      let level = this.id;
+      window.scrollBy({
+        top: level * 500,
+        left:0,
+        behavior:"smooth"
+      });
+    });
+  }
+  
 }

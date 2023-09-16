960c064bfd4830831dde49ba254c6a88308f7477
sachiraghab <sachiraghab@gmail.com>
2023-09-16 21:23:38 +0530

    portfolio styling done

---
+++ script.js
@@ -0,0 +1,7 @@
+const primaryColor = '#fff';
+const secondary = '#f0f0f6';
+const yellowBG = '#ffb400';
+
+function makeActive(){
+  document.getElementById('icon').style.backgroundColor = yellowBG;
+}

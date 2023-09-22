c7dfcbfd5d06db2419bea0017996019f45dc1ae4
sachiraghab <sachiraghab@gmail.com>
2023-09-22 16:15:21 +0530

    project completed

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 window.onload = function (){
-  // const primaryColor = '#fff';
-  // const secondary = '#f0f0f6';
-  // const yellowBG = '#ffb400';
-
   var menuHeader = document.getElementById("header-elem");
   var menuItems = menuHeader.getElementsByClassName("icon-container");
   for(var i=0; i<menuItems.length; i++){
     menuItems[i].addEventListener("click",function (){
       var currentItem = document.getElementsByClassName("active");
       currentItem[0].className = currentItem[0].className.replace("active","");
-      this.className = this.className + " active";
+      this.className = this.className.trim() + " active";
       let level = this.id;
-      window.scrollBy({
-        top: (level-1) * 400,
-        left:0,
-        behavior:"smooth"
-      });
+      let topVal;
+      const desktopScroll = [0,390,900,1300,1560];
+      const tabScroll = [0,2190,2760,3230,3220];
+      const mobileScroll = [0,2080,2820,3440,3638];
+      if(window.innerWidth > 768){
+        topVal = desktopScroll[level-1];
+      }else if(window.innerWidth > 480){
+        topVal = tabScroll[level-1];
+      }else{
+        topVal = mobileScroll[level-1];
+      }
+      window.scrollTo(0,topVal);
     });
-  }
-  
+  }  
 }
